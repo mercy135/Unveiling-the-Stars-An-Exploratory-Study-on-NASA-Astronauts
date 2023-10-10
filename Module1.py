dc567818ffbf769d97e965c28e124b8919b17d0a
mercy135 <42065455+mercy135@users.noreply.github.com>
2023-10-10 13:38:14 +0100

    Create Module1.py

---
+++ Module1.py
@@ -0,0 +1,19 @@
+#.... TASK 1 .....
+/*Import Pandas as 'pd'.
+Read the CSV file nasa.csv into a Pandas DataFrame named 'df'.
+Inspect the data by calling the variable 'df'*/
+
+  
+#--- Import Pandas ---
+import pandas as pd
+#--- Read in dataset ----
+df =  pd.read_csv('nasa.csv')
+# ---WRITE YOUR CODE FOR TASK 1 ---
+#--- Inspect data ---
+df
+
+
+#.... TASK 2 .....
+/*Import Pandas as 'pd'.
+Read the CSV file nasa.csv into a Pandas DataFrame named 'df'.
+Inspect the data by calling the variable 'df'*/
