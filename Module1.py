a84243cc384bb337b4606cb5a7088e443f6f6b04
mercy135 <42065455+mercy135@users.noreply.github.com>
2023-10-13 17:12:08 +0100

    Update Module1.py

--- Module1.py
+++ Module1.py
@@ -39,3 +39,20 @@ df['Birth_Date']= pd.to_datetime(df['Birth_Date'], dayfirst=True)
 df['Death_Date']= pd.to_datetime(df['Death_Date'], dayfirst=True)
 df
 #--- Inspect data ---
+
+
+/* Module 1 - Task 4
+Exporting DataFrame.
+Export the 'df' DataFrame to a CSV file named 'astronauts.csv'.
+Exclude the index from being written to the CSV file by setting 'index' to False.
+Before running this task in your notebook, please ensure that you have previously executed the necessary data processing tasks for the 'df' DataFrame.
+Run the code in Jupyter Notebook once to export the file before proceeding to the 'Run Test'.
+Note: Make sure to hide or comment .to_csv() function code before running test case.*/
+
+/*Module 1 - Task 5
+Data Download, Import, and Database Connection.
+Download the dataset astronauts.csv which is exported in Module 1 - Task 4.
+Create the table on MYSQL using your credentials provided here
+Use the provided login information to access the database by clicking the ""localhost"" link located on the Database Info tab. Once there, you need to upload the required datasets in the specific database mentioned in the database info tab. Rename the table to 'astronauts' using the Operations tab within the database interface and then click on ""Run test"" to complete the task.
+Use the %load_ext sql command to load the SQL extension in your Jupyter Notebook environment. This extension allows you to run SQL commands directly within your notebook.
+Use the %sql magic command to specify the connection string for your MySQL database. Replace <user>, <password>, and <db_name> with your actual database credentials and details.*/
