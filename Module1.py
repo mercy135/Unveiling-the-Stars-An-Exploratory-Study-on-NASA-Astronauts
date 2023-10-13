d2c41a600799f34010d912de93a47f0769576fd3
mercy135 <42065455+mercy135@users.noreply.github.com>
2023-10-13 17:09:59 +0100

    Update Module1.py

--- Module1.py
+++ Module1.py
@@ -24,3 +24,18 @@ null_values=df.isnull().sum()
 #--- Inspect data ---
 null_values
 
+/* Module 1 - Task 3
+Data Cleaning and Filtering Operations.
+Remove rows with missing values in the 'Alma_Mater' column using dropna() and applies the changes directly to the DataFrame 'df' with inplace=True.
+Filter rows where 'Death_Mission' column is null and 'Death_Date' column is not null using the .index method. Then, remove those rows from the DataFrame 'df' by using the .drop() function with the inplace=True parameter.
+Use pd.to_datetime() with the dayfirst=True parameter to convert the 'Birth_Date' and 'Death_Date' columns to datetime format with day-first parsing separately.
+Inspect the data by calling the variable 'df' */
+
+#--- WRITE YOUR CODE FOR TASK 3 ---
+df.dropna(subset=['Alma_Mater'], inplace=True)
+index_to_drop = df[(df['Death_Mission'].isnull()) & (df['Death_Date'].notnull())].index
+df.drop(index_to_drop, inplace=True)
+df['Birth_Date']= pd.to_datetime(df['Birth_Date'], dayfirst=True)
+df['Death_Date']= pd.to_datetime(df['Death_Date'], dayfirst=True)
+df
+#--- Inspect data ---
